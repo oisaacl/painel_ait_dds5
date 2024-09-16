c5e28459ca5df84e2bc132d4543c42e374f88861
ISAAC LUCCA DE PAULA BARBARA <i.barbara@labcetec.net>
2024-09-16 16:55:18 -0300

    Criando tabela de aulas

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './Home.module.css';
 import Cabecalho from '../layout/Cabecalho';
+import TabelaAulas from '../tabelaAulas/TabelaAulas';
+
 
 function Home() {
   return (
     <>
     
     <Cabecalho/>
+    <TabelaAulas/>
+
     
     </>
   )
