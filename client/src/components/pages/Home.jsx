ddef7e67bfbc0c62798b4c278aa8c2a0ed090774
ISAAC LUCCA DE PAULA BARBARA <i.barbara@labcetec.net>
2024-09-20 17:00:26 -0300

    Adicionando Imagens e Estilizando Home

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Home.module.css';
 import Cabecalho from '../layout/Cabecalho';
 import TabelaAulas from '../tabelaAulas/TabelaAulas';
+import LateralImagens from '../LateralImagens/LateralImagens';
 
 
 function Home() {
@@ -10,6 +11,7 @@ function Home() {
     
     <Cabecalho/>
     <TabelaAulas/>
+    <LateralImagens/>
 
     
     </>
--- client/src/components/tabelaAulas/TabelaAulas.jsx
+++ client/src/components/tabelaAulas/TabelaAulas.jsx
@@ -1,48 +1,45 @@
 import { useEffect, useState } from "react";
 import AbreviaData from "./AbreviaData";
 import AbreviaInstrutor from "./AbreviaInstrutor";
+import AbreviaUC from "./AbreviaUC"
+import styles from "./TabelaAulas.module.css"
+
 
 
 function TabelaAulas() {
     const [aulas, setAulas] = useState([]);
+
     useEffect(() => {
         baixarAulas();
     }, [])
 
     async function baixarAulas() {
-
-
-        try {
+        try{
             const resposta = await fetch('http://localhost:5000/aulas', {
                 method: 'GET',
-                Headers: {
-                    'Content-Type': 'aplication/json'
+                headers: {
+                    'Content-type': 'application/json'
                 }
             });
 
             if (!resposta) {
-
-                throw new Error('Erro ao buscar aulas');
-
+                throw new Error('Erro ao buscar');
             }
 
             const consulta = await resposta.json();
             setAulas(consulta);
 
         } catch (error) {
-            console.log("Error ao consultar aulas", error)
+            console.log('Error ao consultar aulas', error)
         }
-
-
     }
-
     return (
-        <div>
 
-            <table>
+        <div className={styles.aulas}>
+            <table className={styles.tabelaAulas}>
                 <thead>
                     <tr>
-                        <th>Inicio</th>
+                        <th>Início</th>
                         <th>Fim</th>
                         <th>Turma</th>
                         <th>Instrutor</th>
@@ -53,11 +50,11 @@ function TabelaAulas() {
                 <tbody>
                     {aulas.map((aula) => (
                         <tr key={aula.id}>
-                            <td><AbreviaData data={aula.data_hora_inicio}/></td>
-                            <td><AbreviaData data={aula.data_hora_fim}/></td>
+                            <td><AbreviaData data={aula.data_hora_inicio} /></td>
+                            <td><AbreviaData data={aula.data_hora_fim} /></td>
                             <td>{aula.turma}</td>
-                            <td><AbreviaInstrutor nome={aula.instrutor}/></td>
-                            <td>{aula.unidade_curricular}</td>
+                            <td><AbreviaInstrutor nome={aula.instrutor} /></td>
+                            <td><AbreviaUC nome={aula.unidade_curricular} /></td>
                             <td>{aula.ambiente}</td>
                         </tr>
                     ))}
